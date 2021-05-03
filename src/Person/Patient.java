2a0ee65d8ec14c1fb65faff30432f3349dcf9b06
Emruur <emreu02@gmail.com>
2021-05-03 22:30:22 +0300

    Persona ID ve Nationality eklemesi

--- src/Person/Patient.java
+++ src/Person/Patient.java
@@ -52,8 +52,8 @@ public class Patient extends Person{
     // constructors
     public Patient(){}
     // simple - just initializes
-    public Patient(String name, String email,String telNo){
-        super(name, email,telNo);
+    public Patient(String name, String email,String telNo,String ID, String nationality){
+        super(name, email,telNo,ID,nationality);
         
         //info = new GeneralInfo(this);
         doctors = new ArrayList<Doctor>();
@@ -64,8 +64,8 @@ public class Patient extends Person{
     }
 
     // complete constructor - if something does not exist, put null (?)
-    public Patient(String name, String email, boolean inIcu, Hospital icuHospital, String city, String country, String address, Disease[] diseases,String telNo){
-        super(name, email,telNo);
+    public Patient(String name, String email, boolean inIcu, Hospital icuHospital, String city, String country, String address, Disease[] diseases,String telNo,String ID, String nationality){
+        super(name, email,telNo,ID,nationality);
         
         this.inIcu = inIcu;
         if(inIcu && icuHospital != null){
